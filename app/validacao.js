13f22f254bbb46f74c144c4c399a2e6871cb250c
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-08-18 15:26:02 -0300

    Tela de fim de jogo

--- app/validacao.js
+++ app/validacao.js
@@ -2,8 +2,17 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     const numero = +chute;
 
     if (chuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div>Valor inválido!</div>';
-        return;
+        if (chute.toUpperCase() === "GAME OVER") {
+            document.body.innerHTML =`
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "red";
+        } else {
+            elementoChute.innerHTML += '<div>Valor inválido!</div>';
+            return;
+        }
     }
 
     if (numeroForMaiorOuMenorQueONumeroPermitido(numero)) {
